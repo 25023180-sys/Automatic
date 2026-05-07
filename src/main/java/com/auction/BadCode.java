a8f89223f66ae630168add4038aebfbb49e25418
unknown <25023180@vnu.edu.vn>
2026-05-07 17:13:54 +0700

    fix checkstyle

--- src/main/java/com/auction/BadCode.java
+++ src/main/java/com/auction/BadCode.java
@@ -1,25 +1,55 @@
 package com.auction;
 
-public class BadCode {
+/**
+ * Class representing a simple bank account with basic operations.
+ * This class is made final to satisfy Checkstyle's DesignForExtension rule.
+ */
+public final class BadCode {
 
+    /** The current balance of the account. */
     private int balance;
 
-    public BadCode(int initialBalance) {
+    /** The magic number for demo purposes. */
+    private static final int MAGIC_HUNDRED = 100;
+
+    /**
+     * Constructs a new BadCode instance with an initial balance.
+     *
+     * @param initialBalance the starting balance of the account
+     */
+    public BadCode(final int initialBalance) {
         this.balance = initialBalance;
     }
 
-    public int deposit(int amount){
+    /**
+     * Deposits an amount into the account.
+     *
+     * @param amount the amount to deposit
+     * @return the new balance
+     */
+    public int deposit(final int amount) {
         System.out.println("amount: " + amount);
-        int x=100;
+        int x = MAGIC_HUNDRED;
         balance = balance + amount;
         return balance;
     }
 
-    public int withdraw(int amount) {
+    /**
+     * Withdraws an amount from the account.
+     *
+     * @param amount the amount to withdraw
+     * @return the new balance
+     */
+    public int withdraw(final int amount) {
         balance = balance - amount;
         return balance;
     }
 
+    /**
+     * Gets the current balance.
+     *
+     * @return the current balance
+     */
     public int getBalance() {
         return balance;
     }
